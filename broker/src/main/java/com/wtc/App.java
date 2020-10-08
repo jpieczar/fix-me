5acc864d521c272b9c18891fb02c57fc1a1c092b
Lebo <lpetsoan@student.wethinkcode.co.za>
2020-10-08 17:27:11 +0200

    Cleaning up wat i have so far.

--- broker/src/main/java/com/wtc/App.java
+++ broker/src/main/java/com/wtc/App.java
@@ -1,13 +1,36 @@
 package com.wtc;
 
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
 /**
- * Hello world!
+ * The broker client, is the one making requests to the market client with 
+ * buy or sell messages.
  *
+ * For now it just establishes a connection, gets the id, and send all the lines the user types
+ * to the server.
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument.");
+            return;
+        }
+
+        try (var socket = new Socket(args[0], 5000)) {
+            System.out.println("Press CTRL-D to stop close the connection.");
+            var scanner = new Scanner(System.in);
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+            
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+                System.out.println(in.nextLine());
+            }
+            out.println(scanner.nextLine());
+            System.out.println(in.nextLine());
+        }
     }
 }
---
+++ router/src/main/java/com/wtc/handlers/Handler.java
@@ -0,0 +1,77 @@
+package com.wtc.handlers;
+
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+import com.wtc.utils.*;
+
+/**
+ * Task handler for all the clients connecting to the router.
+ * 
+ * For now this only considers the brokers, Later is should keep in ming
+ * that markets and brokers connect from different ports on the router.
+ *
+ * */
+public class Handler implements Runnable {
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+
+    public Handler(Socket socket){
+        this.socket = socket;
+    }
+
+    /**
+     * After accepting the connection, this will keep listening to the
+     * client until it either disconnects or sends a quit operation.
+     * 
+     *TODO [ ] Add a way to process and validate the syntax of each message.
+           [ ] Find  proper way to handle the client disconnecting
+           [ ] Figure out how the Chain Of Operation would be used here.
+     * */
+    @Override
+    public void run() {
+        try {
+            initConnection();
+
+            // get the input stream of the client.
+            in = new Scanner(socket.getInputStream());
+            
+            // Get input until the client disconnects from the server.
+            while (true) {
+                // get Message
+                System.out.println(in.nextLine());
+
+                // send response to the client.
+                IDTable.getTable().sendMessage(socket.getPort(), "Message recieve.");
+            }
+        } catch (Exception E) {
+            System.out.println(E);
+        }
+        finally {
+            // Remove the socket from the table of clients.
+            IDTable.getTable().removeClient(socket.getPort());
+
+            //close the socket.
+            try{
+                socket.close();
+            } catch (IOException e){
+                System.err.println("Why would this give an error when closing a socket.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Initiates finishes connection with the client by adding them to the table and
+     * giving them an ID. 
+     * */
+    private void initConnection() throws IOException {
+        IDTable.getTable().addNewClient(socket);
+        IDTable.getTable().sendMessage(socket.getPort(), "Connection was succesfull");
+
+    }
+    
+}
