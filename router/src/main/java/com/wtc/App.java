84f82f6b9e2bc5c10390d938effefbe14a795615
Lebo <lpetsoan@student.wethinkcode.co.za>
2020-10-08 15:23:13 +0200

    Fixed the compilation problem

---
+++ router/src/main/java/com/wtc/App.java
@@ -0,0 +1,19 @@
+package com.wtc;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is going great man.");
+    }
+}
