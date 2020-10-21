0bcb55c1bee76998ea2543eeeab62df10161fedf
jmichael <jmichael@student.wethinkcode.co.za>
2020-10-21 12:46:56 +0000

    Finished and works. Working on fixes in core

---
+++ Jen/router/src/main/java/jmichael/fixme/router/MessageRouter.java
@@ -0,0 +1,93 @@
+package jmichael.fixme.router;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import jmichael.fixme.core.FixHandler;
+import jmichael.fixme.core.DataHandler;
+import jmichael.fixme.core.messages.SystemMailMan;
+import jmichael.fixme.core.validators.AllFixTagsValidator;
+import jmichael.fixme.core.messages.MailMan;
+import jmichael.fixme.core.validators.ChecksumChecker;
+import jmichael.fixme.router.handler.MessageHandler;
+
+
+public class MessageRouter {
+
+    public static final String RESET_COLOUR  = "\u001B[0m";
+    public static final String BRIGHT_BG_WHITE  = "\u001B[107m";
+    public static final String RED = "\u001B[31m";
+    public static final String BRIGHT_BLUE   = "\u001B[36m";
+    public static final String BRIGHT_GREEN  = "\u001B[92m";
+    private final AtomicInteger id = new AtomicInteger(FixHandler.START_ID);
+    private final Map<String, AsynchronousSocketChannel> routingTable = new ConcurrentHashMap<>();
+    private final Map<String, String> failedToSendMessages = new ConcurrentHashMap<>();
+
+    private void runRouter() {
+        System.out.println(BRIGHT_BLUE+BRIGHT_BG_WHITE +"Message Router Status   ::" + " [ON]");
+        System.out.println(BRIGHT_BLUE+BRIGHT_BG_WHITE +"Market Server port      ::" + " [5001]");
+        System.out.println(BRIGHT_BLUE+BRIGHT_BG_WHITE +"Broker Server port      ::" + " [5000]");
+        System.out.print(RESET_COLOUR);
+
+        try {
+            final MailMan mailMan = getMessageHandler();
+
+            final AsynchronousServerSocketChannel brokersListener = AsynchronousServerSocketChannel
+                    .open()
+                    .bind(new InetSocketAddress(FixHandler.HOST, 5000));
+            brokersListener.accept(null,
+                    new SuccessHandler(brokersListener, routingTable, id, mailMan));
+
+            final AsynchronousServerSocketChannel marketsListener = AsynchronousServerSocketChannel
+                    .open()
+                    .bind(new InetSocketAddress(FixHandler.HOST, 5001));
+            marketsListener.accept(null,
+                    new SuccessHandler(marketsListener, routingTable, id, mailMan));
+        } catch (IOException e) {
+            System.out.println(RED + "\n[ERROR] :: Socket was not opened.");
+        }
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ignored) {
+            }
+            tryToSendFailedMessages();
+        }
+    }
+
+    private void tryToSendFailedMessages() {
+        if (!failedToSendMessages.isEmpty()) {
+            System.out.println(RED + "[RESENDING MESSAGES] ...");
+            failedToSendMessages.keySet().removeIf(targetName -> {
+                final AsynchronousSocketChannel targetChannel = routingTable.get(targetName);
+                if (targetChannel != null) {
+                    System.out.println(BRIGHT_GREEN+ " [Message] " + targetName + " - [SENDING]");
+                    System.out.print(RESET_COLOUR);
+                    DataHandler.sendMessage(targetChannel, failedToSendMessages.get(targetName));
+                    return true;
+                }
+                return false;
+            });
+        }
+    }
+
+    private MailMan getMessageHandler() {
+        final MailMan mailMan = new SystemMailMan();
+        final MailMan mandatoryTagsValidator = new AllFixTagsValidator();
+        final MailMan checksumValidator = new ChecksumChecker();
+        final MailMan messageParser = new MessageHandler(routingTable, failedToSendMessages);
+        mailMan.setNext(mandatoryTagsValidator);
+        mandatoryTagsValidator.setNext(checksumValidator);
+        checksumValidator.setNext(messageParser);
+        return mailMan;
+    }
+
+    public static void main(String[] args) {
+        new MessageRouter().runRouter();
+    }
+}
---
+++ Jen/router/src/main/java/jmichael/fixme/router/handler/MessageHandler.java
@@ -0,0 +1,40 @@
+package jmichael.fixme.router.handler;
+
+import jmichael.fixme.core.FixHandler;
+import jmichael.fixme.core.enums.FixTagIdentifiers;
+import jmichael.fixme.core.DataHandler;
+import jmichael.fixme.core.messages.MainMessageHandler;
+
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Map;
+
+import static jmichael.fixme.router.MessageRouter.BRIGHT_BLUE;
+import static jmichael.fixme.router.MessageRouter.BRIGHT_GREEN;
+
+public class MessageHandler extends MainMessageHandler {
+
+    public static final String RED = "\u001B[31m";
+    private final Map<String, AsynchronousSocketChannel> routingTable;
+    private final Map<String, String> failedMessages;
+
+    public MessageHandler(Map<String, AsynchronousSocketChannel> routingTable,
+                          Map<String, String> failedMessages) {
+        this.routingTable = routingTable;
+        this.failedMessages = failedMessages;
+    }
+
+    @Override
+    public void validateMessage(AsynchronousSocketChannel clientChannel, String message) {
+        System.out.println(BRIGHT_GREEN + "\n[VALIDATING INPUT] :: " + BRIGHT_BLUE + message);
+        final String targetName = FixHandler.getValueByTag(message, FixTagIdentifiers.RECIPIENT);
+        final AsynchronousSocketChannel targetChannel = routingTable.get(targetName);
+        if (targetChannel != null) {
+            DataHandler.sendMessage(targetChannel, message);
+            super.validateMessage(clientChannel, message);
+        } else {
+            DataHandler.sendInternalMessage(clientChannel,
+                    RED+ "\n[ERROR] :: No id found that matches " + targetName + "\n" + "[RESENDING]...");
+            failedMessages.put(targetName, message);
+        }
+    }
+}
